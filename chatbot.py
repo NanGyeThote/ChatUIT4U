20c6d133d290d65b4acbb612c8557b727903d567
Nan Gye Thote <kywzinth3nt@gmail.com>
2024-12-10 03:47:31 +0630

    adding rq and st

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ import json
 import pickle
 import numpy as np
 import nltk
+import streamlit as st
 from keras.models import load_model
 
 # Initialize lemmatizer and load data
@@ -12,14 +13,14 @@ words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('uit_chat.h5')
 
+# Function to clean and lemmatize the sentence
 def clean_up_sentence(sentence):
-    """Tokenize and lemmatize the input sentence."""
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
+# Function to create the bag of words
 def bag_of_words(sentence):
-    """Create a bag of words for the given sentence."""
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
     for w in sentence_words:
@@ -27,12 +28,10 @@ def bag_of_words(sentence):
             bag[words.index(w)] = 1
     return np.array(bag)
 
+# Function to predict the class of the input sentence
 def predict_class(sentence):
-    """Predict the class of the given sentence using the model."""
     bow = bag_of_words(sentence)
-    # print(f"Bag of words: {bow}")  # Debugging line
     res = model.predict(np.array([bow]))[0]
-    # print(f"Model output: {res}")  # Debugging line
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
 
@@ -40,11 +39,11 @@ def predict_class(sentence):
     return_list = [{'intent': classes[r[0]], 'probability': str(r[1])} for r in results]
     return return_list
 
+# Function to get a response based on the predicted intent
 def get_response(intents_list, intents_json):
-    """Get a response based on the predicted intent."""
     if not intents_list:
         return "Sorry, I didn't understand that."
-
+    
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     
@@ -54,15 +53,21 @@ def get_response(intents_list, intents_json):
     
     return "Sorry, I didn't find an appropriate response."
 
-# Console interface
-print("GO! Bot is running!")
+# Streamlit App
 
-while True:
-    message = input("You: ").strip()  # Trim input to handle extra spaces
-    if message.lower() in ["exit", "quit", "bye"]:
-        print("Bot: Goodbye!")
-        break
-    ints = predict_class(message)
-    res = get_response(ints, intents)
-    # print("Classes:", classes)  # This should print the list of intent tags
-    print("Bot:", res)
+st.title("UIT Chatbot")
+st.write("Welcome to the University of Information Technology (UIT) Yangon chatbot!")
+
+# Input box for user messages
+user_input = st.text_input("You: ", "")
+
+# Handle Exit phrases (exit, bye, goodbye, etc.)
+exit_phrases = ["exit", "bye", "goodbye", "see you", "quit", "later"]
+if user_input.lower() in exit_phrases:
+    st.write("Bot: Goodbye! Have a great day.")
+else:
+    # Process user input and get response
+    if user_input:
+        ints = predict_class(user_input)
+        response = get_response(ints, intents)
+        st.write(f"Bot: {response}")
